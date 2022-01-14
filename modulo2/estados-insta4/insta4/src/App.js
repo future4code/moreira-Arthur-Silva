408cb188698365d00ae7ef2b16c43f43b086a657
Arthut Kelvim <kelvimarthur@gmail.com>
2022-01-13 23:23:19 -0300

    Alterações no trabalho insta4

--- modulo2/estados-insta4/insta4/src/App.js
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -9,28 +9,109 @@ const MainContainer = styled.div`
   align-items: center;
 `
 
+const InputEdit = styled.input`
+  height: 30px;
+  border: 2px solid black;
+  margin: 10px;
+  background-color: lightblue;
+  border-radius: 5px;
+  box-shadow: 2px 2px 3px black;
+  padding:0px 5px;
+  font-size: 20px;
+  font-style: italic;
+`
+const DivPai = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 class App extends React.Component {
+  state={
+    listaUsuariosInsta: [
+      {
+        nomeUsuario:'paulinha',
+        fotoUsuario:'https://picsum.photos/50/50',
+        fotoPost:'https://picsum.photos/200/150',
+      },
+      {
+        nomeUsuario:'julia',
+        fotoUsuario:'https://picsum.photos/50/50',
+        fotoPost:'https://picsum.photos/200/151',
+      },
+      {
+        nomeUsuario:'joanna',
+        fotoUsuario:'https://picsum.photos/50/50',
+        fotoPost:'https://picsum.photos/200/156',
+      }
+    ],
+    valorNomeUsuario:'',
+    valorFotoUsuario:'',
+    valorFotoPost:''
+  }      
+      
+  
+  /*------------------------------------------------------------------------------------*/ 
+
+  adicionaPost = () => {
+    const adicionarPost = {
+      nomeUsuario: this.state.valorNomeUsuario,
+      fotoUsuario: this.state.valorFotoUsuario,
+      fotoPost: this.state.valorFotoPost
+    };
+    const novaLista = [...this.state.listaUsuariosInsta, adicionarPost ];
+    this.setState({
+      listaUsuariosInsta: novaLista,
+      valorNomeUsuario: "",
+      valorFotoUsuario: "",
+      valorFotoPost: ""
+    }); 
+  };
+  onChangeNome = (event) => {
+    this.setState({ valorNomeUsuario: event.target.value });
+  };
+  onChangeFoto = (event) => {
+    this.setState({ valorFotoUsuario: event.target.value });
+  };
+  onChangePost = (event) => {
+    this.setState({ valorFotoPost: event.target.value });
+  };
+  /*-------------------------------------------------------------------------------------*/ 
   render() {
+    const listaDePosts = this.state.listaUsuariosInsta.map((post) => {
+      return (
+        <Post
+        nomeUsuario={post.nomeUsuario} 
+        fotoUsuario={post.fotoUsuario} 
+        fotoPost={post.fotoPost}
+        />
+      );
+    });
     return (
-      <MainContainer>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50'}
-          fotoPost={'https://picsum.photos/200/150'}
-        />
-        <Post
-          nomeUsuario={'Julia'}
-          fotoUsuario={'https://hypescience.com/wp-content/uploads/2010/03/feliz.jpg'}
-          fotoPost={'https://img.quizur.com/f/img5effc916550f53.01612839.jpg?lastEdited=1593821469'}
-        />
-        <Post
-          nomeUsuario={'Joana'}
-          fotoUsuario={'https://www.familia.com.br/wp-content/uploads/2019/12/Pessoas-felizes-n%C3%A3o-t%C3%AAm-tempo-para-criticar-a-vida-dos-outros.jpg'}
-          fotoPost={'https://i.pinimg.com/474x/77/fa/fd/77fafd2bf86762baf726198fd04caf4d.jpg'}
-        />
-      </MainContainer>
-    )
+      <div>
+        <DivPai>
+          <InputEdit
+            value={this.state.valorNomeUsuario}           
+            onChange={this.onChangeNome}
+            placeholder={"nome"}
+          />
+          <InputEdit            
+            value={this.state.valorFotoUsuario}           
+            onChange={this.onChangeFoto}
+            placeholder={"Sua foto"}
+          />
+          <InputEdit           
+            value={this.state.valorFotoPost}           
+            onChange={this.onChangePost}
+            placeholder={"URL"}
+          />
+          <button onClick={this.adicionaPost}>Adicionar</button>
+        </DivPai>
+        <MainContainer>{listaDePosts}</MainContainer>
+      </div>
+    );
+    
   }
+    
 }
 
 export default App;
